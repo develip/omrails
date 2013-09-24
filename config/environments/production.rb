5812c93b3c6087c98214e65eca0fb0d6e33f633d
develip <nataliewy.ip@gmail.com>
2013-09-25 00:56:06 +0800

    Add s3_host_name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,7 @@ Omrails::Application.configure do
       :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      :s3_host_name => 's3.amazonaws.com',
       :s3_endpoint => 's3.amazonaws.com'
     }
   }
